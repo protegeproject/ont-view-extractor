fe29cf189d726e038113c3ed6b283e62a6362ae1
nyulas <2441046+csnyulas@users.noreply.github.com>
2020-02-18 13:28:44 +0000

    Moved the organization of post-coordination axes in different list, based on the nature of their value set (custom scale, fixed scale, hierarchical), which was duplicated in various places of several projects into the ICDContentModel and ICDContentModelConstants.

--- src/main/java/org/logical/defs/ICDPostCoordinationMaps.java
+++ src/main/java/org/logical/defs/ICDPostCoordinationMaps.java
@@ -77,47 +77,7 @@ public class ICDPostCoordinationMaps {
 		/***** External causes axes - ignored for now *****/
 		
 	}};
-		
-	public static final List<String> fixedScalePCPropIds = new ArrayList<>(
-		Arrays.asList(
-				ICDContentModelConstants.PC_AXIS_ETIOLOGY_CAUSALITY,
-				ICDContentModelConstants.PC_AXIS_TOPOLOGY_LATERALITY,
-				ICDContentModelConstants.PC_AXIS_TOPOLOGY_RELATIONAL, 
-				ICDContentModelConstants.PC_AXIS_TOPOLOGY_DISTRIBUTION,
-				ICDContentModelConstants.PC_AXIS_TOPOLOGY_REGIONAL,
-				ICDContentModelConstants.PC_AXIS_INJURY_QUALIFIER_FRACTURE_QUALIFIER_FRACTURE_SUBTYPE, 
-				ICDContentModelConstants.PC_AXIS_INJURY_QUALIFIER_FRACTURE_QUALIFIER_OPEN_OR_CLOSED, 
-				ICDContentModelConstants.PC_AXIS_INJURY_QUALIFIER_FRACTURE_QUALIFIER_JOINT_INVOLVEMENT_IN_FRACTURE_SUBTYPE,
-				ICDContentModelConstants.PC_AXIS_INJURY_QUALIFIER_TYPE_OF_INJURY,
-				ICDContentModelConstants.PC_AXIS_INJURY_QUALIFIER_BURN_QUALIFIER,
-				ICDContentModelConstants.PC_AXIS_CONSCIOUSNESS_MEASURE_DURATION_OF_COMA,
-				ICDContentModelConstants.PC_AXIS_LEVEL_OF_CONSCIOUSNESS
-		)
-	);
-	
-	
-	public static final List<String> hierarchicalPCPropIds = new ArrayList<>(
-		Arrays.asList(
-				ICDContentModelConstants.PC_AXIS_TEMPORALITY_TIME_IN_LIFE,
-				ICDContentModelConstants.PC_AXIS_ETIOLOGY_INFECTIOUS_AGENT,
-				ICDContentModelConstants.PC_AXIS_ETIOLOGY_CHEMICAL_AGENT, 
-				ICDContentModelConstants.PC_AXIS_ETIOLOGY_MEDICATION,
-				ICDContentModelConstants.PC_AXIS_HISTOPATHOLOGY,
-				ICDContentModelConstants.PC_AXIS_SPECIFIC_ANATOMY
-		)
-	);
-	
-	
-	public static final List<String> scalePCPropIds = new ArrayList<>(
-		Arrays.asList(
-				ICDContentModelConstants.PC_AXIS_HAS_SEVERITY,
-				ICDContentModelConstants.PC_AXIS_HAS_ALT_SEVERITY1,
-				ICDContentModelConstants.PC_AXIS_HAS_ALT_SEVERITY2,
-				ICDContentModelConstants.PC_AXIS_TEMPORALITY_COURSE,
-				ICDContentModelConstants.PC_AXIS_TEMPORALITY_PATTERN_AND_ONSET
-		)
-	);
-	
+
 	
 	private OWLModel owlModel;
 	
@@ -134,9 +94,9 @@ public class ICDPostCoordinationMaps {
 	}
 
 	private void fillMaps() {
-		fixedScalePCProps = fillMap(fixedScalePCPropIds);
-		scalePCProps = fillMap(scalePCPropIds);
-		hierarchyPCProps = fillMap(hierarchicalPCPropIds);
+		fixedScalePCProps = fillMap(ICDContentModelConstants.FIXED_SCALE_PC_AXES_PROPERTIES_LIST);
+		scalePCProps = fillMap(ICDContentModelConstants.SCALE_PC_AXES_PROPERTIES_LIST);
+		hierarchyPCProps = fillMap(ICDContentModelConstants.HIERARCHICAL_PC_AXES_PROPERTIES_LIST);
 		
 		fillXChapter2PcPropMap();
 	}
